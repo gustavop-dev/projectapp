3f705113d1fcf10c01d0fbc86ea0473326545bb5
Gustavo Adolfo Pérez Pérez <87864256+gustavop-dev@users.noreply.github.com>
2024-11-03 20:22:25 -0500

    feat: implement basic PWA setup with offline support (#7)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import { fileURLToPath, URL } from 'url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -15,10 +15,37 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Project App',
+        short_name: 'ProjectApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/static/frontend/img/icons/icon-logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/static/frontend/img/icons/icon-logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{js,css,png,jpg,svg,webp,woff2,woff}'],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
