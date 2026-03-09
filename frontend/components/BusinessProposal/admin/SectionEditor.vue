e1f519d772782a60143be96a8dcd8f6b014f7d52
Gustavo Pérez <guperezp@unal.edu.co>
2026-03-09 22:45:58 +0000

    fix: allow deleting Módulo Administrativo group in proposal editor

--- frontend/components/BusinessProposal/admin/SectionEditor.vue
+++ frontend/components/BusinessProposal/admin/SectionEditor.vue
@@ -565,7 +565,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
               </svg>
             </button>
-            <button v-if="group.id !== 'views' && group.id !== 'components' && group.id !== 'features' && group.id !== 'integrations_api' && group.id !== 'admin_module'"
+            <button v-if="group.id !== 'views' && group.id !== 'components' && group.id !== 'features' && group.id !== 'integrations_api'"
               type="button" class="text-xs text-red-500 hover:text-red-700 ml-2" @click="form.groups.splice(gIdx, 1)">Eliminar</button>
           </div>
         </div>
